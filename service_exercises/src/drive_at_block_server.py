bb21022dbceff75d123853a447e5cee6499a80e3
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-10-04 13:57:15 +0100

    Add files via upload

--- service_exercises/src/drive_at_block_server.py
+++ service_exercises/src/drive_at_block_server.py
@@ -8,21 +8,46 @@ from sensor_msgs.msg import LaserScan
 
 class Drive_at_block:
 
-    def __init__(self):       
+    def __init__(self):
+        self.closest_object_angle=0       
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.sub = rospy.Subscriber('/scan', LaserScan, self.callback_laser)
-        vel = Twist()
+        self.vel = Twist()
 
     def callback_laser(self, LaserMsg):
-        self.Laser_scan_array = LaserMsg
-        self.closest_object_range=min(self.Laser_scan_array.ranges)
+        Laser_scan_array = LaserMsg
+        closest_object_range=min(Laser_scan_array.ranges)
         #print(closest_object_range)
-        self.closest_object_angle=self.Laser_scan_array.ranges.index(min(self.Laser_scan_array.ranges))
-        #print(closest_object_angle)   
+        self.closest_object_angle=Laser_scan_array.ranges.index(min(Laser_scan_array.ranges))
+        print(self.closest_object_angle) 
+        if self.closest_object_angle <= 5:
+            self.vel.angular.z=0
+            self.pub.publish(self.vel)
+            print("Target Aquired") 
+            self.vel.linear.x=1
+            self.pub.publish(self.vel)          
+        else:
+            if self.closest_object_angle < 360:
+                self.vel.angular.z=0.5
+                self.pub.publish(self.vel)
+                print("Turning left")
+            else:
+                if self.closest_object_angle >= 715:
+                    self.vel.angular.z=0
+                    self.pub.publish(self.vel)
+                    print("Target Aquired")
+                    self.vel.linear.x=1
+                    self.pub.publish(self.vel)                         
+                else:
+                    self.vel.angular.z=-0.5
+                    self.pub.publish(self.vel)
+                    print("Turning right")                
+
+        
 
 
-    def callback_move(self, service_request):
-        print(self.closest_object_angle)
+    #def callback_move(self, service_request):
+        #print(self.closest_object_angle)
 
 
 

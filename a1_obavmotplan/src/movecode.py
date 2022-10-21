11cb0ff7c81350eb1d7b3f6274887bbf95149998
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-10-21 23:59:18 +0100

    Update movecode.py

--- a1_obavmotplan/src/movecode.py
+++ a1_obavmotplan/src/movecode.py
@@ -8,6 +8,7 @@ from tf.transformations import euler_from_quaternion
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
 import math
+from matplotlib import pyplot as plt
 
 class Drive_at_block:
 
@@ -16,7 +17,7 @@ class Drive_at_block:
         # self.x_goal=int(rospy.get_param("/go_robot_go/x_goal"))
         # self.y_goal=int(rospy.get_param("/go_robot_goal/y_goal"))
         # self.goal=(self.x_goal,self.y_goal)
-        self.goal=(80,70)
+        self.goal=(60,30)
         self.grid=np.zeros((100,100))
         print(self.grid)
         self.closest_object_angle=0       
@@ -30,20 +31,22 @@ class Drive_at_block:
         self.linearx=0
         self.lineary=0
         self.robot_position=([[0],[0]])
-        self.q_goal=(80,30)
         self.q_0=(0,0)
         self.robot_position_grid=([[49],[49]])
         self.max_speed=1
         self.ctrl_c=False
         rospy.on_shutdown(self.shutdown) 
         self.on_target=False
-    
+
  
         
     def shutdown(self):
+        plt.imshow(self.grid)
+        plt.show()
         self.vel.angular.z=0
         self.vel.linear.x=0
         self.pub.publish(self.vel)
+        plt.imshow(self.grid)
         print("shutting down")
 
     def callback_laser(self, LaserMsg):
@@ -55,11 +58,11 @@ class Drive_at_block:
             # if self.real_angle[i] >= 360:
             #     self.real_angle[i] = self.real_angle[i] - 360
             # self.real_angle[i]= self.real_angle[i]*180/math.pi #convert it to radians for sin cos you fucking idiot
-            theta= (i/2) *180/math.pi
+            theta= (i/2) *math.pi/180
 
 
 
-            if self.Laser_scan_array.ranges[i] <2: #self.Laser_scan_array.range_max:
+            if self.Laser_scan_array.ranges[i] <3: #self.Laser_scan_array.range_max:
                 #print(self.Laser_scan_array.ranges[i])
                 if self.Laser_scan_array.ranges[i] > self.Laser_scan_array.range_min:
                     bodyframe=([[(self.Laser_scan_array.ranges[i]*math.cos(theta))], [self.Laser_scan_array.ranges[i]*math.sin(theta)]])
@@ -119,30 +122,33 @@ class Drive_at_block:
         self.potential_field()
     def potential_field(self):
         p_0=2 #barrier around object in meters
-        F_rep=0
+        q_goal=self.goal
+        F_rep=(0,0)
         k_att=1
         k_rep=1
         q=self.robot_position_grid #robot position on grid
-        p_goal=math.sqrt((self.q_goal[0]-self.robot_position_grid[0])^2+(self.q_goal[1]-self.robot_position_grid[1])^2) #distance from robot to goal
+        p_goal=math.sqrt(pow(q_goal[0]-self.robot_position_grid[0],2)+pow(q_goal[1]-self.robot_position_grid[1],2)) #distance from robot to goal
         #Attractive
-        U_att=1/2 *k_att*p_goal^2
-        F_att=-k_att*(q-self.q_goal)
+        U_att=1/2 *k_att*pow(p_goal,2)
+        
+        F_att=-k_att*((q[0]-q_goal[0]),(q[1]-q_goal[1]))
         
         for i in range(-20,20): #takes a 2m range around the robot, based on the map though
             for j in range(-20,20):
-                if self.grid[self.robot_position_grid[0]+i,self.robot_position_grid[1]+j] == 1:
-                    p_q=math.sqrt((self.robot_position_grid[0]+i)^2+(self.robot_position_grid[1]+j)^2)
+                if self.grid[int(self.robot_position_grid[0])+i,int(self.robot_position_grid[1])+j] == 1:
+                    p_q=math.sqrt(pow(self.robot_position_grid[0]+i,2)+pow(self.robot_position_grid[1]+j,2))
                         #Repulsive
                     if p_q<=p_0:
-                        U_rep=1/2 * k_rep*(((1/p_q)-(1/p_0))^2)
-                        F_rep=F_rep+k_rep*((1/p_q)-(1/p_0))*(1/p_q^2)*((q-self.q_0)/p_q)
+                        U_rep=1/2 * k_rep*(pow((1/p_q)-(1/p_0),2))
+                        F_rep=F_rep+k_rep*((1/p_q)-(1/p_0))*(1/pow(p_q,2))*((q-self.q_0)/p_q)
                     else:
                         U_rep=0
-                        F_rep=F_rep
+        print(F_att) 
+        print(-k_att*(q[0]-q_goal[0]),(q[1]-q_goal[1]))               
         potential_vector= (F_att[1]+F_rep[1],F_att[0]+F_rep[0])
         print(potential_vector)
         self.potential_angle=np.arctan2(potential_vector[1],potential_vector[0])
-        self.potential_force= self.max_speed/math.sqrt(potential_vector[0]^2,potential_vector[1])
+        self.potential_force= self.max_speed/math.sqrt(pow(potential_vector[0],2),potential_vector[1])
         self.turn()
 
     def turn(self):
@@ -180,7 +186,7 @@ class Drive_at_block:
         self.linearx=odom_data.pose.pose.position.x
         self.lineary=odom_data.pose.pose.position.y
         self.robot_position=([[self.linearx], [self.lineary]]) #position on not on array
-        self.robot_position_grid=([[self.linearx*10+49], [self.lineary*10+49]])
+        self.robot_position_grid=(self.linearx*10+49, self.lineary*10+49)
         #print(self.robot_position)
         linearz=odom_data.pose.pose.position.z
         quaternion_orientation= odom_data.pose.pose.orientation
@@ -189,6 +195,9 @@ class Drive_at_block:
             self.real_yaw =yaw+2*math.pi
         else:
             self.real_yaw = yaw #angle of robot relative to world frame
+        
+        if self.robot_position_grid[0] >=75 or self.robot_position_grid[1] >=75 or self.robot_position_grid[0]<=25 or self.robot_position_grid[1]<=25:
+            print("Warning, robot close to edge of map, will fall off into void soon...")
 
         #print(self.real_yaw)
         #print(f"x is {self.linearx:.2f}, y is {self.lineary:.2f}, z is {linearz:.2f}, Yaw is {self.real_yaw:.2f}")
@@ -208,4 +217,3 @@ if __name__ == '__main__':
     rospy.loginfo('its working dipshit')
     Drive_at_block()
     rospy.spin()
-
